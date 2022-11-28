4c67c4e652e68b8bbe49c4975add35741215f67f
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-11-28 21:48:49 +0530

    Initial commit

---
+++ src/main/java/com/dhivakar/helpercli/HelpercliApplication.java
@@ -0,0 +1,13 @@
+package com.dhivakar.helpercli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpercliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpercliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhivakar/helpercli/HelpercliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhivakar.helpercli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpercliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
